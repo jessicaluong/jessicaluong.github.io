d3a3439993d852b073a931e51d365c100dc5fb16
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2024-04-19 15:20:12 -0700

    Fix publish.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  
-    reactStrictMode: true,
+    output: "export",
+    images: {
+      loader: 'akamai',
+      path: '',
+    },
+    assetPrefix: './',
   };
   
   export default nextConfig;
