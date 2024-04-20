59d1556a2d7dcc79df1585bf8f88416b7e8bc2e8
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2024-04-19 17:20:40 -0700

    Fix images not showing up

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: "export",
+    basePath: isProd ? '/jessicaluong.github.io' : '',
+    assetPrefix: isProd ? '/jessicaluong.github.io/' : '',
 };    
   
-  export default nextConfig;
+export default nextConfig;
