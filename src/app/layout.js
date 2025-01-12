7d08c118ed802a67628b987917c0dbbcddf97bca
jessicaluong <96930184+jessicaluong@users.noreply.github.com>
2025-01-12 15:22:28 -0800

    Fix font not loading

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,9 +2,10 @@ import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
 const roboto = Roboto_Mono({
-  weight: '400',
-  subsets: ['latin'],
-})
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 export const metadata = {
   title: "jessicaluong.github.io",
   description: "Jessica Luong Portfolio",
